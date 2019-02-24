f26611a2c43200aec743642db9eab3156447a62c
WinstonLHS <winstonlimhs@gmail.com>
2019-02-24 16:50:01 +0800

    comparators

---
+++ javascript_Tutorial/javascripts/comparestuff.js
@@ -0,0 +1,8 @@
+function compare(a,b){
+    
+    if (a===b) console.log(a + " and " + b + " are strictly equal");
+    else if (a==b) console.log(a + " and " + b + " are equal");
+    else console.log(a + " and " + b + " are not equal");
+}
+
+compare(1,'one');
