9993e508ebb5bfa1861c5a40f01eb466e871aa29
WinstonLHS <winstonlimhs@gmail.com>
2019-02-27 08:23:59 +0800

    clean code

--- javascript_Tutorial/javascripts/comparestuff.js
+++ javascript_Tutorial/javascripts/comparestuff.js
@@ -5,4 +5,20 @@ function compare(a,b){
     else console.log(a + " and " + b + " are not equal");
 }
 
-compare(1,'one');
+
+function compar2(a,b){
+    
+    if (a!=b) console.log(typeof a + " " + a + " and " + typeof b + " " +  b + " are not equal");
+    else console.log(typeof a + " " + a + " and " + typeof b + " " + b + " are not NOT equal");
+    if (a!==b) console.log(typeof a + " " + a + " and " + typeof b + " " +  b + " are strictly not equal");
+}
+
+function compar3(num){
+    if (num < 5) return "Tiny";
+    else if (num < 10) return "Small";
+    else if (num < 15) return "Medium";
+    else if (num < 20) return "Large";
+    else if (num >= 20) return "Huge";
+}
+
+console.log(compar3(40));
